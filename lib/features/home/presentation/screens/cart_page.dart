676bdca601fedc3f7c969d29af72b4d312ef88e2
ahmedalnagdy18 <ahmedalnagdy188@gmail.com>
2024-08-07 16:23:45 +0300

    finish add to cart page

--- lib/features/home/presentation/screens/cart_page.dart
+++ lib/features/home/presentation/screens/cart_page.dart
@@ -24,95 +24,148 @@ class _AddToCart extends StatelessWidget {
         backgroundColor: Colors.white,
         title: const Text('Cart'),
       ),
-      body: BlocBuilder<AddToCartCubit, AddtoCartState>(
-        builder: (context, state) {
-          if (state is CartState) {
-            if (state.cartItems.isEmpty) {
-              return const Center(
-                child: Text('Your cart is empty'),
-              );
-            }
-            return ListView.builder(
-              itemCount: state.cartItems.length,
-              itemBuilder: (context, index) {
-                final item = state.cartItems[index];
+      body: Column(
+        children: [
+          Expanded(
+            child: BlocBuilder<AddToCartCubit, AddtoCartState>(
+              builder: (context, state) {
+                if (state is CartState) {
+                  if (state.cartItems.isEmpty) {
+                    return const Center(
+                      child: Text('Your cart is empty'),
+                    );
+                  }
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: state.cartItems.length,
+                          itemBuilder: (context, index) {
+                            final item = state.cartItems[index];
 
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
-                  child: Container(
-                    width: double.infinity,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      color: Colors.white,
-                    ),
-                    clipBehavior: Clip.antiAlias,
-                    child: Row(
-                      children: [
-                        Container(
-                          constraints: const BoxConstraints(minWidth: 100),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: Colors.grey.shade300),
-                          child: Image.network(
-                            item.images?[0] ?? '',
-                            errorBuilder: (context, error, stackTrace) =>
-                                const Icon(Icons.error),
-                          ),
-                        ),
-                        const SizedBox(width: 30),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              MainTextWidget(
-                                text: item.title ?? '',
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              MainTextWidget(
-                                text: item.category ?? '',
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              Expanded(
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 14),
+                              child: Container(
+                                width: double.infinity,
+                                height: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(6),
+                                  color: Colors.white,
+                                ),
+                                clipBehavior: Clip.antiAlias,
                                 child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Text(
-                                      '+ \$ ${(item.price! - item.discountPercentage!).clamp(0, double.infinity).toStringAsFixed(2)}',
-                                      style: const TextStyle(
-                                        color: Colors.green,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 16,
+                                    Container(
+                                      constraints:
+                                          const BoxConstraints(minWidth: 100),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          color: Colors.grey.shade300),
+                                      child: Image.network(
+                                        item.images?[0] ?? '',
+                                        errorBuilder:
+                                            (context, error, stackTrace) =>
+                                                const Icon(Icons.error),
                                       ),
                                     ),
-                                    IconButton(
-                                        onPressed: () {
-                                          context
-                                              .read<AddToCartCubit>()
-                                              .removeItemFromCart(item);
-                                        },
-                                        icon: const Icon(Icons.delete)),
-                                    const SizedBox(),
+                                    const SizedBox(width: 30),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          MainTextWidget(
+                                            text: item.title ?? '',
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          MainTextWidget(
+                                            text: item.category ?? '',
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          Expanded(
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Text(
+                                                  '+ \$ ${(item.price! - item.discountPercentage!).clamp(0, double.infinity).toStringAsFixed(2)}',
+                                                  style: const TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.w400,
+                                                    fontSize: 16,
+                                                  ),
+                                                ),
+                                                IconButton(
+                                                    onPressed: () {
+                                                      context
+                                                          .read<
+                                                              AddToCartCubit>()
+                                                          .removeItemFromCart(
+                                                              item);
+                                                    },
+                                                    icon: const Icon(
+                                                        Icons.delete)),
+                                                const SizedBox(),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
                                   ],
                                 ),
                               ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
+                            );
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: BlocBuilder<AddToCartCubit, AddtoCartState>(
+                          builder: (context, state) {
+                            if (state is CartState) {
+                              final totalPrice = context
+                                  .read<AddToCartCubit>()
+                                  .calculateTotalPrice();
+                              return Text.rich(
+                                TextSpan(
+                                  text: 'Total: ',
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text:
+                                          '\$ ${totalPrice.toStringAsFixed(2)}',
+                                      style: const TextStyle(
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }
+                            return Container();
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                }
+                return const Center(
+                  child: Text('Your cart is empty'),
                 );
               },
-            );
-          }
-          return const Center(
-            child: Text('Your cart is empty'),
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
