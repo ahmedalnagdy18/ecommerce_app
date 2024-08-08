0460d65e8f16e58961a455829b3333c81b3f03b6
ahmedalnagdy18 <ahmedalnagdy188@gmail.com>
2024-08-08 17:32:04 +0300

    add internet check connection feature

--- lib/features/home/presentation/screens/main_home_page.dart
+++ lib/features/home/presentation/screens/main_home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_test/features/home/presentation/screens/cart_page.dart';
 import 'package:flutter_application_test/features/home/presentation/screens/bottom_navigation_bar.dart';
@@ -8,38 +9,80 @@ import 'package:flutter_application_test/features/home/presentation/screens/my_p
 class MainPage extends StatefulWidget {
   const MainPage({super.key, this.name});
   final List? name;
+
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
-  List name = [];
-  int selectedindex = 0;
+  List<Widget> screens = [];
+  int selectedIndex = 0;
+  bool _hasInternet = true;
 
   @override
   void initState() {
-    name.addAll([
+    super.initState();
+    _checkInternetConnectivity();
+    screens.addAll([
       const MyHomepage(),
       const AddToCart(),
       const ExplorePage(),
       const MyProfilePage(),
     ]);
-    super.initState();
+  }
+
+  Future<void> _checkInternetConnectivity() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        setState(() {
+          _hasInternet = true;
+        });
+      } else {
+        setState(() {
+          _hasInternet = false;
+        });
+      }
+    } on SocketException catch (_) {
+      setState(() {
+        _hasInternet = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: name.elementAt(selectedindex),
-        bottomNavigationBar: BottomNavigationBarWidget(
-          currentIndex: selectedindex,
-          onTap: _onItemTapped,
-        ));
+      body: _hasInternet
+          ? screens.elementAt(selectedIndex)
+          : const Scaffold(
+              backgroundColor: Colors.white,
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.error,
+                      color: Colors.red,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                        textAlign: TextAlign.center,
+                        'No internet connection \n Please check your connection.'),
+                  ],
+                ),
+              ),
+            ),
+      bottomNavigationBar: BottomNavigationBarWidget(
+        currentIndex: selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 
   void _onItemTapped(int value) {
     setState(() {
-      selectedindex = value;
+      selectedIndex = value;
     });
   }
 }
